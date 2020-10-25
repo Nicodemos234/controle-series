57e6e4d170150c4a610954efd70ff1438029cef0
Nicodemos234 <nicodemos54324@gmail.com>
2020-10-25 12:32:31 -0300

    named routes and style improvement

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -27,7 +27,7 @@ class SeriesController extends Controller
             'mensagem',
             "Série {$serie->id} criada com sucesso {$serie->nome}"
         );
-        return redirect('/series');
+        return redirect()->route('listar_series');
     }
 
     public function destroy(Request $request) {
@@ -36,6 +36,6 @@ class SeriesController extends Controller
             'mensagem',
             "Serie removida com sucesso"
         );
-        return redirect('/series');
+        return redirect()->route('listar_series');
     }
 }
