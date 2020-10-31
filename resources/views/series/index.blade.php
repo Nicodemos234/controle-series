2821e23aef42d1aa1729c86ecc66fd86f8d12bb4
Nicodemos234 <nicodemos54324@gmail.com>
2020-10-31 14:04:20 -0300

    Authenticate added

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -7,7 +7,9 @@
 @section('conteudo')
     @include('mensagem', ['mensagem' => $mensagem])
 
-    <a href="{{ route('form_criar_serie') }}" class="btn btn-dark mb-2">Adicionar</a>
+    @auth
+        <a href="{{ route('form_criar_serie') }}" class="btn btn-dark mb-2">Adicionar</a>
+    @endauth
 
     <ul class="list-group">
         @foreach ($series as $serie)
@@ -25,18 +27,22 @@
                 </div>
 
                 <span class="d-flex">
-                    <button class="btn btn-info btn-sm mr-1" onclick="toggleInput({{ $serie->id }})">
-                        <i class="fas fa-edit"></i>
-                    </button>
+                    @auth
+                        <button class="btn btn-info btn-sm mr-1" onclick="toggleInput({{ $serie->id }})">
+                            <i class="fas fa-edit"></i>
+                        </button>
+                    @endauth
                     <a href="/series/{{ $serie->id }}/temporadas" class="btn btn-info btn-sm mr-1">
                         <i class="fas fa-external-link-alt"></i>
                     </a>
-                    <form method="post" action="/series/remover/{{ $serie->id }}"
-                        onsubmit="return confirm('Tem certeza que deseja remover {{ addslashes($serie->nome) }}?')">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></button>
-                    </form>
+                    @auth
+                        <form method="post" action="/series/remover/{{ $serie->id }}"
+                            onsubmit="return confirm('Tem certeza que deseja remover {{ addslashes($serie->nome) }}?')">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></button>
+                        </form>
+                    @endauth
                 </span>
             </li>
 
--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,36 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
+
 Route::get('/series', 'SeriesController@index')
     ->name('listar_series');
 Route::get('/series/criar', 'SeriesController@create')
-    ->name('form_criar_serie');
-Route::post('/series/criar', 'SeriesController@store');
-Route::delete('/series/remover/{id}', 'SeriesController@destroy');
-Route::post('/series/{id}/editaNome', 'SeriesController@editaNome');
+    ->name('form_criar_serie')
+    ->middleware('autenticador');
+Route::post('/series/criar', 'SeriesController@store')
+    ->middleware('autenticador');
+Route::delete('/series/remover/{id}', 'SeriesController@destroy')
+    ->middleware('autenticador');
+Route::post('/series/{id}/editaNome', 'SeriesController@editaNome')
+    ->middleware('autenticador');
 
 Route::get('/series/{serieId}/temporadas', 'TemporadasController@index');
 
 Route::get('/temporadas/{temporada}/episodios', 'EpisodiosController@index');
-Route::post('/temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir');
+Route::post('/temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir')
+    ->middleware('autenticador');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/entrar', 'EntrarController@index');
+Route::post('/entrar', 'EntrarController@entrar');
+
+Route::get('/registrar', 'RegistroController@create');
+Route::post('/registrar', 'RegistroController@store');
+
+Route::get('/sair', function () {
+    Auth::logout();
+    return redirect('/entrar');
+});
